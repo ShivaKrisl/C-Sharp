5a844d4e6f6b518447591116441a400f681be670
Shiva Krishna Beeraboina <106444427+ShivaKrisl@users.noreply.github.com>
2024-10-27 23:38:27 +0530

    Add files via upload
    Added Basic Codes

---
+++ LearningCodeLanguage/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using LearningCodeLanguage.OopsDemo;
+
+namespace LearningCodeLanguage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console Methods */
+            SystemConsoleDemo.ConsoleMethods();
+
+            /* Variables */
+            VariablesDemo.Variables();
+
+            /* DataTypes */
+            DataTypesDemo.BytesDemo();
+
+            /* Operators */
+            OperatorsDemo.ArithmeticOperatorsDemo();
+
+            /* Operators Assignment */
+            OperatorsAssignment.CalculateAreaOfCircle();
+            OperatorsAssignment.FeetToCentimeterConverter();
+            OperatorsAssignment.MoneyRoundOffValue();
+            OperatorsAssignment.SecondsConverter();
+
+            /* Conditionals */
+            ConditionalsAssignment.HeightCategory();
+            ConditionalsAssignment.LargestOfThreeNumbers();
+            ConditionalsAssignment.MarksFinder();
+
+            /* Looping */
+            LoopingDemo.WhileLoopDemo();
+            LoopingDemo.DoWhileLoopDemo();
+            LoopingDemo.ForLoopDemo();
+            LoopingDemo.ForEachLoopDemo(); 
+            LoopingDemo.GotoDemo();
+
+            /*Class*/
+            ClassDemo obj = new ClassDemo();
+            obj.Display();
+        }
+    }
+}
